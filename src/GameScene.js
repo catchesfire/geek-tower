962d5cfde010891d29c40ff0ad3ab85e168b84bd
Vikes2 <muczynski.pawel@gmail.com>
2018-12-04 00:00:43 +0100

    Added slide animation. Left move is broken

--- src/GameScene.js
+++ src/GameScene.js
@@ -12,7 +12,7 @@ class GameScene extends Phaser.Scene {
         this.load.image("ground", "../assets/blue-bar.gif");
         this.load.image("groundVertex", "../assets/blue-bar-vertex.gif");
         // this.load.image("geek", "../assets/red-rect.gif");
-        this.load.spritesheet("geek", "../assets/geek.png", {frameWidth: 88, frameHeight: 132});
+        this.load.spritesheet("geek", "../assets/geek.png", {frameWidth: 88, frameHeight: 95});
     }
     create(){
         this.add.image('ground', 200, 200);
@@ -21,20 +21,51 @@ class GameScene extends Phaser.Scene {
         platform2.create( 687 , 320, "groundVertex" );
         platform.create( 640 , 685, "ground" );
         platform.create( 640 , 450, "ground" );
-        this.geek = new Geek(this,200,300,"geek");
+        this.geek = new Geek(this,360,360,"geek");
         this.physics.add.collider(this.geek, platform);
         this.physics.add.collider(this.geek, platform2);
         this.geek.body.checkCollision.up = false;
         this.geek.body.checkCollision.left = false;
         this.geek.body.checkCollision.right = false;
         this.geek.body.setBounceX(2);
-        
+
+        // this.cameras.main.startFollow(this.geek);
+        this.cameras.main.setDeadzone(720, 670);
+        this.cameras.main.startFollow(this.geek);
+        // this.cameras.main.setBounds(0,0,720,2000);
 
         this.anims.create({
-            key : "geek_walk",
-            frames: this.anims.generateFrameNumbers('geek',{start: 2, end: 4} ),
-            frameRate: 8,
+            key : "geek_walk_right",
+            frames: this.anims.generateFrameNumbers('geek',{start:1, end:7, frames:[1,2,3,7,4]} ),
+            frameRate: 10,
             repeat: -1
+        })
+        this.anims.create({
+            key : "geek_stand",
+            frames: this.anims.generateFrameNumbers('geek',{start:0, end:0} ),
+            frameRate: 1,
+            repeat: -1
+            
+        })
+        this.anims.create({
+            key : "geek_jump",
+            frames: this.anims.generateFrameNumbers('geek',{start:5, end:5} ),
+            frameRate: 1,
+            repeat: -1
+        })
+        this.anims.create({
+            key : "geek_jump_right",
+            frames: this.anims.generateFrameNumbers('geek',{start:6, end:6} ),
+            frameRate: 1,
+            repeat: -1
+            
+        })
+        this.anims.create({
+            key : "geek_slide_right",
+            frames: this.anims.generateFrameNumbers('geek',{start:8, end:8} ),
+            frameRate: 1,
+            repeat: -1
+            
         })
 
 
--- src/Geek.js
+++ src/Geek.js
@@ -3,8 +3,8 @@ const FacingRight = 14;
 const FacingUp = 11;
 const FacingDown = 12;
 
-class Geek extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, key){
+class Geek extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, key) {
         super(scene, x, y, key);
         this.scene = scene;
         this.scene.add.existing(this);
@@ -13,47 +13,71 @@ class Geek extends Phaser.GameObjects.Sprite{
         this.isJumping = false;
         this.isBouncing = false;
         this.tempTime = null;
-        this.setScale(0.5);
+        this.setScale(0.77);
         this.direction = 0;
         this.speedModifierX = 1;
         this.speedModifierY = 1;
         this.speed = 800;
         this.currentSpeed = 0;
         this.jumpSpeed = -1300;
+        this.isSliding = { left: false, right: false }
+
         // this.body.setMaxVelocity(650);
-        
+
     }
-    update(time, delta, cursors){
-        if(!this.isBouncing) {
+    update(time, delta, cursors) {  
+        this.isSliding.left= false;
+        this.isSliding.right = false;
+        
+        if (!this.isBouncing) {
             this.direction = 0;
-        }else if(this.tempTime < time){
-            this.isBouncing = false;            
+            
+        } else if (this.tempTime < time) {
+            this.isBouncing = false;
             this.speedModifierY = 1;
             this.speedModifierX = 1;
         }
+        
+        if (cursors.right.isDown && !this.isBouncing) {
+            if (this.body.facing == FacingLeft) {
+                this.currentSpeed += 80;
+                if(this.currentSpeed < 0){
+                    this.isSliding.left = true;
+                }else{
+                    this.currentSpeed = 0;
+                }
 
-        if(cursors.left.isDown && !this.isBouncing) {
-            
-            if(this.body.facing == FacingRight){
+            }else{
                 this.currentSpeed = 0;
-                
+            }
+            this.direction = 1;
+        }
+        if (cursors.left.isDown && !this.isBouncing) {
+
+            console.log("------")
+            console.log(FacingRight)
+            console.log(this.body.facing)
+            console.log("------")
+            if (this.body.facing == FacingRight) {
+                this.currentSpeed -=80;
+                if(this.currentSpeed > 0){
+                    this.isSliding.right = true;
+                }else{
+                    this.currentSpeed = 0;
+                }
+
+            } else{
+                this.currentSpeed = 0;
             }
             this.direction = -1;
         }
 
-        if(cursors.right.isDown && !this.isBouncing) {
-            if(this.body.facing == FacingLeft){
-                this.currentSpeed = 0;
-                
-            }
-            this.direction = 1;
+
+        if (this.body.blocked.down) {
+            this.isJumping = false;
         }
 
-        if(this.body.blocked.down){
-            this.isJumping= false;
-        }
-
-        if(this.body.blocked.left || this.body.blocked.right) {
+        if (this.body.blocked.left || this.body.blocked.right) {
             this.direction *= -1;
             this.isBouncing = true;
             this.speedModifierY *= 1.2;
@@ -61,34 +85,100 @@ class Geek extends Phaser.GameObjects.Sprite{
             this.tempTime = time + 10 * delta;
         }
 
-        if(cursors.up.isDown) {
-            if(!this.isJumping){
+        if (cursors.up.isDown) {
+
+
+            if (!this.isJumping) {
                 this.body.setVelocityY(this.jumpSpeed * this.speedModifierY);
-                this.isJumping= true;
+                this.isJumping = true;
             }
         }
 
-        let tempSpeed;
-        if(this.currentSpeed >= this.speed){
-            this.currentSpeed = this.speed;
-        }else{
-            this.currentSpeed += 50;
+        // if(!this.isSliding.left && !this.isSliding.right){
+        //     this.currentSpeed = this.currentSpeed >= this.speed ? this.speed : this.currentSpeed + 1000;
+        // }
+        if(!this.isSliding.left && !this.isSliding.right){
+            if(this.currentSpeed >= this.speed){
+                this.currentSpeed = this.speed;
+            }else{
+                this.currentSpeed+= 500;
+            }
         }
-        this.body.setVelocityX(this.direction * this.currentSpeed * this.speedModifierX);
-
         
-        switch(this.direction) {
+
+
+        if (this.direction == 0) {
+            if (this.body.velocity.x < 0) {
+                if (this.body.velocity.x + 50 > 0) {
+
+                    this.body.setVelocityX(0)
+                    
+                } else {
+                    this.body.setVelocityX(this.body.velocity.x + 80);
+                    this.isSliding.left= true;
+                }
+            }
+            if (this.body.velocity.x > 0) {
+                if (this.body.velocity.x - 50 < 0) {
+
+                    this.body.setVelocityX(0)
+                } else {
+                    this.body.setVelocityX(this.body.velocity.x - 80);
+                    this.isSliding.right=true;
+                }
+            }
+
+        } else {
+            let tempSpeed = this.direction * this.currentSpeed * this.speedModifierX;
+            console.log(tempSpeed)
+            if(this.isSliding.right || this.isSliding.left){
+                tempSpeed *= (-1);
+                console.log(tempSpeed)
+                
+                this.body.setVelocityX(tempSpeed);
+
+            }else{
+
+                this.body.setVelocityX(tempSpeed);
+            }
+        }
+
+        switch (this.direction) {
             case -1:
                 // go left
                 break;
             case 0:
+                if (!this.isJumping) {
+                    if(this.isSliding.left){
+                        //to do
+                    }else if(this.isSliding.right){
+                        this.anims.play("geek_slide_right",true);
+                    }else{
+                        this.anims.play("geek_stand", true);
+                    }
+
+                } else {
+                    this.anims.play("geek_jump", true);
+                }
                 break;
             case 1:
                 // go right
+                if (!this.isJumping) {
+                    if(this.isSliding.left){
+                        //to do
+                    }else if(this.isSliding.right){
+                        this.anims.play("geek_slide_right",true);
+                    }else{
+                        this.anims.play("geek_walk_right", true);
+                    }
+
+                } else {
+                    this.anims.play("geek_jump_right", true);
+                }
                 break;
         }
 
-        
+
     }
 
 
