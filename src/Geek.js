cb2ab1b02644f3ac3f7cd00ca67d87bd336655da
Vikes2 <muczynski.pawel@gmail.com>
2018-12-07 23:58:47 +0100

    Improve geek movement.

--- src/Geek.js
+++ src/Geek.js
@@ -10,177 +10,137 @@ class Geek extends Phaser.GameObjects.Sprite {
         this.scene.add.existing(this);
         this.scene.physics.world.enable(this);
         this.body.collideWorldBounds = true;
+        //this.body.onWorldBounds = true;
+
         this.isJumping = false;
-        this.isBouncing = false;
         this.tempTime = null;
         this.setScale(0.77);
-        this.direction = 0;
-        this.speedModifierX = 1;
         this.speedModifierY = 1;
-        this.speed = 800;
-        this.currentSpeed = 0;
+        this.maxSpeed = 800;
+        this.currentSpeed= 0;
         this.jumpSpeed = -1300;
-        this.isSliding = { left: false, right: false }
-
-        // this.body.setMaxVelocity(650);
-
+        this.isSliding = { left: false, right: false };
+        this.isBouncing = { left: false, right: false };
+        this.flag = false;
     }
     update(time, delta, cursors) {  
-        this.isSliding.left= false;
-        this.isSliding.right = false;
+        this.flipX = false;
         
-        if (!this.isBouncing) {
-            this.direction = 0;
-            
-        } else if (this.tempTime < time) {
-            this.isBouncing = false;
+        //console.log(this.body.velocity.y);
+        let controlStage = 0;
+        this.isJumping = false;
+        this.isSliding = { left: false, right: false }
+        
+        if(cursors.right.isUp && this.body.velocity.x > 0 && this.body.blocked.down){
+            this.isSliding.right = true;
+        }
+        if(cursors.left.isUp && this.body.velocity.x < 0 && this.body.blocked.down){
+            this.isSliding.left = true;
+        }
+        if (!this.body.blocked.down){
+            this.isJumping = true;
+        }
+        if (this.tempTime < time) {
+            this.isBouncing.left = false;
+            this.isBouncing.right = false;
+            this.flag = false;
             this.speedModifierY = 1;
-            this.speedModifierX = 1;
         }
-        
-        if (cursors.right.isDown && !this.isBouncing) {
-            if (this.body.facing == FacingLeft) {
-                this.currentSpeed += 80;
-                if(this.currentSpeed < 0){
-                    this.isSliding.left = true;
-                }else{
-                    this.currentSpeed = 0;
-                }
-
-            }else{
-                this.currentSpeed = 0;
-            }
-            this.direction = 1;
-        }
-        if (cursors.left.isDown && !this.isBouncing) {
-
-            console.log("------")
-            console.log(FacingRight)
-            console.log(this.body.facing)
-            console.log("------")
-            if (this.body.facing == FacingRight) {
-                this.currentSpeed -=80;
-                if(this.currentSpeed > 0){
-                    this.isSliding.right = true;
-                }else{
-                    this.currentSpeed = 0;
-                }
-
-            } else{
-                this.currentSpeed = 0;
-            }
-            this.direction = -1;
-        }
-
-
-        if (this.body.blocked.down) {
-            this.isJumping = false;
-        }
-
-        if (this.body.blocked.left || this.body.blocked.right) {
-            this.direction *= -1;
-            this.isBouncing = true;
-            this.speedModifierY *= 1.2;
-            this.speedModifierX *= 0.8;
+        if (this.body.blocked.left){
+            this.isBouncing.left = true;
+            this.tempTime = time + 10 * delta;
+        } 
+        if(this.body.blocked.right) {
+            this.isBouncing.right = true;
             this.tempTime = time + 10 * delta;
         }
-
-        if (cursors.up.isDown) {
-
-
-            if (!this.isJumping) {
+        if(this.isBouncing.left){
+            if(!this.flag){
+                this.flag = true;
+                this.currentSpeed = Math.abs(this.body.velocity.x);
+                if(this.currentSpeed > this.maxSpeed){
+                    this.currentSpeed =this.maxSpeed * 0.8;
+                }
+                this.speedModifierY = 1.2;
+                if(this.body.velocity.y < 0){
+                    this.body.setVelocityY(this.body.velocity.y *this.speedModifierY);
+                }
+            }
+            this.body.setVelocityX(this.currentSpeed);
+        }else if(this.isBouncing.right){
+            if(!this.flag){
+                this.flag = true;
+                this.currentSpeed = Math.abs(this.body.velocity.x);
+                if(this.currentSpeed > this.maxSpeed){
+                    this.currentSpeed =this.maxSpeed * 0.8;
+                }
+                this.speedModifierY = 1.2;
+                if(this.body.velocity.y < 0){
+                    this.body.setVelocityY(this.body.velocity.y *this.speedModifierY);
+                }
+            }
+            this.body.setVelocityX(-this.currentSpeed);
+        }else{
+            if(this.isSliding.left){
+                this.currentSpeed = Math.abs(this.body.velocity.x) - 80;
+                if(this.currentSpeed <= 0){
+                    this.currentSpeed = 0
+                }
+                this.body.setVelocityX(-this.currentSpeed);
+            }else if(this.isSliding.right){
+                this.currentSpeed = Math.abs(this.body.velocity.x) - 80;
+                if(this.currentSpeed <= 0){
+                    this.currentSpeed = 0
+                }
+                this.body.setVelocityX(this.currentSpeed);
+            }else{
+                if (cursors.left.isDown ){
+                    this.currentSpeed = Math.abs(this.body.velocity.x) + 100;
+                    if(this.currentSpeed > this.maxSpeed){
+                        this.currentSpeed =this.maxSpeed;
+                    }
+                    this.body.setVelocityX(-this.currentSpeed);
+                }
+                if (cursors.right.isDown ){
+                    this.currentSpeed = Math.abs(this.body.velocity.x) + 100;
+                    if(this.currentSpeed > this.maxSpeed){
+                        this.currentSpeed =this.maxSpeed;
+                    }
+                    this.body.setVelocityX(this.currentSpeed);
+                }
+            }
+        }
+        if (cursors.up.isDown ){
+            if(!this.isJumping ){
                 this.body.setVelocityY(this.jumpSpeed * this.speedModifierY);
-                this.isJumping = true;
             }
         }
 
-        // if(!this.isSliding.left && !this.isSliding.right){
-        //     this.currentSpeed = this.currentSpeed >= this.speed ? this.speed : this.currentSpeed + 1000;
-        // }
-        if(!this.isSliding.left && !this.isSliding.right){
-            if(this.currentSpeed >= this.speed){
-                this.currentSpeed = this.speed;
+
+
+        // ======= ANIM
+        if(this.isJumping){
+            if(this.body.velocity.x < 0){
+                this.flipX = true;
+                this.anims.play("geek_jump_right", true);
+            }else if(this.body.velocity.x > 0){
+                this.anims.play("geek_jump_right", true);
             }else{
-                this.currentSpeed+= 500;
+                this.anims.play("geek_jump", true);
             }
+        }else if(this.isSliding.left){
+            this.flipX = true;
+            this.anims.play("geek_slide_right", true);
+        }else if(this.isSliding.left){
+            this.anims.play("geek_slide_right", true);
+        }else if(this.body.velocity.x > 0){
+            this.anims.play("geek_walk_right", true);
+        }else if(this.body.velocity.x < 0){
+            this.flipX = true;
+            this.anims.play("geek_walk_right", true);
+        }else{
+            this.anims.play("geek_stand", true);
+            
         }
-        
-
-
-        if (this.direction == 0) {
-            if (this.body.velocity.x < 0) {
-                if (this.body.velocity.x + 50 > 0) {
-
-                    this.body.setVelocityX(0)
-                    
-                } else {
-                    this.body.setVelocityX(this.body.velocity.x + 80);
-                    this.isSliding.left= true;
-                }
-            }
-            if (this.body.velocity.x > 0) {
-                if (this.body.velocity.x - 50 < 0) {
-
-                    this.body.setVelocityX(0)
-                } else {
-                    this.body.setVelocityX(this.body.velocity.x - 80);
-                    this.isSliding.right=true;
-                }
-            }
-
-        } else {
-            let tempSpeed = this.direction * this.currentSpeed * this.speedModifierX;
-            console.log(tempSpeed)
-            if(this.isSliding.right || this.isSliding.left){
-                tempSpeed *= (-1);
-                console.log(tempSpeed)
-                
-                this.body.setVelocityX(tempSpeed);
-
-            }else{
-
-                this.body.setVelocityX(tempSpeed);
-            }
-        }
-
-        switch (this.direction) {
-            case -1:
-                // go left
-                break;
-            case 0:
-                if (!this.isJumping) {
-                    if(this.isSliding.left){
-                        //to do
-                    }else if(this.isSliding.right){
-                        this.anims.play("geek_slide_right",true);
-                    }else{
-                        this.anims.play("geek_stand", true);
-                    }
-
-                } else {
-                    this.anims.play("geek_jump", true);
-                }
-                break;
-            case 1:
-                // go right
-                if (!this.isJumping) {
-                    if(this.isSliding.left){
-                        //to do
-                    }else if(this.isSliding.right){
-                        this.anims.play("geek_slide_right",true);
-                    }else{
-                        this.anims.play("geek_walk_right", true);
-                    }
-
-                } else {
-                    this.anims.play("geek_jump_right", true);
-                }
-                break;
-        }
-
-
     }
-
-
-
 }
